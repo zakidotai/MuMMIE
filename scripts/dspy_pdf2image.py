adf402b39f438ce9a6435180ae2c62598c7194d4
abhijeetgangan <abhijeet.gangan261997@gmail.com>
2025-09-11 20:20:36 -0700

    Better prompts

--- scripts/dspy_pdf2image.py
+++ scripts/dspy_pdf2image.py
@@ -5,18 +5,18 @@ import os
 import base64
 from io import BytesIO
 import pandas as pd
+from mummie.prompts import compostion_property_prompt
 
 api_key = os.environ.get("GOOGLE_API_KEY") or os.environ.get("GEMINI_API_KEY")
-dspy.configure(lm=dspy.LM("gemini/gemini-2.5-flash", api_key=api_key))
+dspy.configure(lm=dspy.LM("gemini/gemini-2.5-flash", api_key=api_key, max_tokens=
+                          8192))
 
 page_qa = dspy.Predict(PageQA)
 
-pages = convert_from_path("../data/pdf/RU_2383502_C1.pdf", dpi=300)
+pages = convert_from_path("../data/pdf/CN1308591A.pdf", dpi=300)
 total_responce = {}
 for i, pil_image in enumerate(pages[1:]):
-    question = "Give me a list of compositions if they are mentioned in the page if not return empty list. \
-        The compsition list should be of the type list[dict[str, float]]. Make sure they are realistic compositions containing real elements. \
-        If something is not parse properly in the PDF, return the best guess of the composition."
+    question = compostion_property_prompt
     buf = BytesIO()
     pil_image.convert("RGB").save(buf, format="PNG")
     data_url = f"data:image/png;base64,{base64.b64encode(buf.getvalue()).decode('utf-8')}"
@@ -25,7 +25,7 @@ for i, pil_image in enumerate(pages[1:]):
     total_responce[i] = raw_answer
     print(f"Page {i+1}: {raw_answer}")
 
-df = pd.read_excel('../data/excel/RU2383502_table-37098.xlsx')
+df = pd.read_excel('../data/excel/CN1308591_table-24104.xlsx')
 column_dict = dict()
 mapper = dict()
 columns = df.columns.tolist()
